10739017403a8a31964906436777f122c65e8f99
Drslope-99 <enamsifon@gmail.com>
2026-04-02 15:09:40 +0100

    added the todo collection interface

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,17 @@
-console.clear();
-console.log("welcome to typescript");
+import { TodoItem } from "./todoItem";
+import { TodoCollection } from "./todoCollection";
+
+let todos: TodoItem[] = [
+  new TodoItem(1, "Buy Flowers"),
+  new TodoItem(2, "Get Shoes"),
+  new TodoItem(3, "Collect Tickets"),
+  new TodoItem(4, "Call Joe", true),
+];
+
+let collection: TodoCollection = new TodoCollection("Charles", todos);
+
+console.log(`${collection.userName}'s Todo List:`);
+
+let newId: number = collection.addTodo("Go for a run");
+let todoItem: TodoItem | undefined = collection.getTodoById(newId);
+todoItem?.printDetails();
